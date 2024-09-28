a853b2b9d0201d50e3e79b43f5af614b73dbc333
WIN-B8ADQ695FQP\sobol <sobol4156@gmail.com>
2024-09-28 22:22:47 +0300

    fix

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('db_network', 'root', 'gdeta235R', {
-  host: 'mysql',
-  port: 3007,
-  dialect: 'mysql'
-})
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+});
 
-sequelize.authenticate()
-.then(() => console.log('connected db_network'))
-.catch(err => console.log(`нет конекта к бд ${err}`))
+sequelize
+  .authenticate()
+  .then(() => console.log("connected db_network"))
+  .catch((err) => console.log(`нет конекта к бд ${err}`));
 
-sequelize.sync({ force: false })
-        .then(() => {
-            console.log(`Database & tables synchronised!`)
-        });
+sequelize.sync({ force: false }).then(() => {
+  console.log(`Database & tables synchronised!`);
+});
 
-
-module.exports = sequelize
+module.exports = sequelize;
